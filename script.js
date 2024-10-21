7ff5dc6a5a79b0b4c67ceca00d050d28508d1143
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2024-10-21 12:54:43 +1100

    Solve fibonacci using recursive method

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Iteratively generate a fibonacci sequence up to the 'n' index
+
 function fibs(n) {
   if (n === 1) return [0];
   if (n === 2) return [0, 1];
@@ -9,4 +11,20 @@ function fibs(n) {
   return array;
 }
 
-console.log(fibs(8));
+// console.log(fibs(8));
+
+// Recursively generate a fibonacci sequence up to the 'n' index
+
+function fibsRec(n) {
+  // Handle base cases
+  if (n === 0) return [];
+  if (n === 1) return [0];
+  if (n === 2) return [0, 1];
+  // Handle recursion case
+  const array = fibsRec(n - 1);
+  array.push(array[array.length - 1] + array[array.length - 2]);
+  return array;
+}
+
+console.log(fibsRec(8));
+//should return the array [0, 1, 1, 2, 3, 5, 8, 13].
